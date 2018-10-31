e9ae4f450db0237faa2461db8e642f0cb159e54c
Etienne Stalmans <estalmans@gmail.com>
2018-10-31 14:07:58 +0000

    add seccomp

--- main.go
+++ main.go
@@ -5,13 +5,42 @@ import (
 	"os"
 	"os/exec"
 	"syscall"
+
+	libseccomp "github.com/seccomp/libseccomp-golang"
 )
 
+func whiteList(syscalls []string) {
+	filter, err := libseccomp.NewFilter(libseccomp.ActErrno.SetReturnCode(int16(syscall.EPERM)))
+	if err != nil {
+		fmt.Printf("Error creating filter: %s\n", err)
+	}
+	for _, element := range syscalls {
+		//fmt.Printf("[+] Whitelisting: %s\n", element)
+		syscallID, err := libseccomp.GetSyscallFromName(element)
+		if err != nil {
+			panic(err)
+		}
+		filter.AddRule(syscallID, libseccomp.ActAllow)
+	}
+	filter.Load()
+}
+
 func main() {
-    cmd := exec.Command("id")
+
+	var syscalls = []string{"write", "rt_sigaction", "mmap", "clock_gettime", "clone",
+		"mprotect", "futex", "openat", "access", "rt_sigprocmask", "fstat", "seccomp",
+		"munmap", "wait4", "close", "read", "brk", "set_tid_address", "sigaltstack",
+		"prlimit64", "execve", "sched_getaffinity", "set_robust_list", "arch_prctl",
+		"gettid", "rt_sigreturn", "getpid", "waitid",
+		"readlinkat", "pipe", "pipe2", "exit_group", "connect", "statfs", "socket",
+		"lseek", "getgroups", "getgid", "geteuid", "getegid", "getuid", "exit", "fcntl"}
+
+	whiteList(syscalls)
+
+	cmd := exec.Command("/usr/bin/id")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-				Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-				//Unshareflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	//Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	//Unshareflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
